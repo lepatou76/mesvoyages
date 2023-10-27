4d59a7e0983cc42bffc649b19b278421dc888539
lepatou76 <patrice.ambert@wanadoo.fr>
2023-10-27 19:35:44 +0200

    Phase 10 : ORM (filtrage des enregistrements)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -6,6 +6,7 @@ use App\Repository\VisiteRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Description of VoyagesController
@@ -44,6 +45,20 @@ class VoyagesController extends AbstractController{
      */
     public function sort($champ, $ordre): Response{
         $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
+    /**
+     * @Route("/voyages/recherche/{champ}", name="voyages.findallequal")
+     * @param type $champ
+     * @param Request $request
+     * @return Response
+     */
+    public function findAllEqual($champ, Request $request): Response{
+        $valeur = $request->get("recherche");
+        $visites = $this->repository->findByEqualValue($champ, $valeur);
         return $this->render("pages/voyages.html.twig", [
             'visites' => $visites
         ]);
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -48,9 +48,33 @@ class VisiteRepository extends ServiceEntityRepository
      
     public function findAllOrderBy($champ, $ordre): array{
         return $this->createQueryBuilder('v')
-                ->orderBy("v.".$champ, $ordre)
+                ->orderBy('v.'.$champ, $ordre)
                 ->getQuery()
                 ->getResult();
+    }
+    
+    /**
+     * Enregistrements dont un champ est égal à une valeur
+     * ou tous les enregistrements si la valeuur est vide
+     * @param type $champ
+     * @param type $valeur
+     * @return Visite[]
+     */
+    
+    public function findByEqualValue($champ, $valeur): array{
+        if($valeur==""){
+            return $this->createQueryBuilder('v') // alias de la table
+                ->orderBy('v.'.$champ, 'ASC')
+                ->getQuery()
+                ->getResult();
+        }else{
+            return $this->createQueryBuilder('v') // alias de la table
+                ->where('v.'.$champ.'=:valeur')  
+                ->setParameter('valeur', $valeur)
+                ->orderBy('v.datecreation', 'DESC')
+                ->getQuery()
+                ->getResult();
+        }
     }
 //    public function findByExampleField($value): array
 //    {
