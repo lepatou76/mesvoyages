bc9900295dc23b0219fb21654cfa4d9005f686e0
lepatou76 <patrice.ambert@wanadoo.fr>
2023-10-30 17:59:49 +0100

    Phase 13 : ORM (modification d'un enregistrement, création d'un formulaire)

--- src/Controller/admin/AdminVoyageController.php
+++ src/Controller/admin/AdminVoyageController.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace App\Controller\admin;
 
 use App\Entity\Visite;
+use App\Form\VisiteType;
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -50,5 +52,26 @@ class AdminVoyageController extends AbstractController{
         $visite = $this->repository->find($id);
         $this->repository->remove($visite, true);
         return $this->redirectToRoute('admin.voyages');
+    }
+    
+    /**
+     * @Route("/admin/edit/{id}", name="admin.voyage.edit")
+     * @param int $id
+     * @return Response
+     */
+    public function edit(int $id, Request $request): Response{
+        $visite = $this->repository->find($id);
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+        
+        $formVisite->handleRequest($request);
+        if($formVisite->isSubmitted() && $formVisite->isValid()){
+            $this->repository->add($visite, true);
+            return $this->redirectToRoute('admin.voyages');
+        }     
+        
+        return $this->render("admin/admin.voyage.edit.html.twig", [
+            'visite' => $visite,
+            'formvisite' => $formVisite->createView()
+        ]);        
     }
 }
